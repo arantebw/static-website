b82f583ec186b9513092de8d76ae1965922f67e6
Billy Arante <arantebillywilson@gmail.com>
2023-06-03 18:41:32 +0800

    feat(projects): render the fetched projects to the projects' page

--- scripts/projects.js
+++ scripts/projects.js
@@ -9,5 +9,47 @@ const fetchProjects = async () => {
 
 (async function () {
   const projects = await fetchProjects();
-  console.log(projects);
+  const projectsContainer = document.querySelector("#projects");
+  projects.forEach(project => {
+    // create the project's section
+    const sectionProject = document.createElement("section");
+    sectionProject.setAttribute("class", "project");
+
+    // create the project's snapshot image
+    const img = document.createElement("img");
+    img.setAttribute("src", project.snapshot);
+
+    // create the project header
+    const h3 = document.createElement("h3");
+    const h3Text = document.createTextNode(project.projectName);
+    h3.appendChild(h3Text);
+
+    // create the project's made with items
+    const ul = document.createElement("ul");
+    ul.setAttribute("class", "techs");
+
+    if (project.madeWith) {
+      project.madeWith.forEach(item => {
+        const li = document.createElement("li");
+        const liText = document.createTextNode(item);
+        li.appendChild(liText);
+        ul.appendChild(li);
+      });
+    }
+
+    // create the description paragraph
+    const descriptionPara = document.createElement("p");
+    descriptionPara.setAttribute("class", "description");
+    const descriptionText = document.createTextNode(project.description);
+    descriptionPara.appendChild(descriptionText);
+
+    // append the elements to the project's section
+    sectionProject.appendChild(img);
+    sectionProject.appendChild(h3);
+    sectionProject.appendChild(ul);
+    sectionProject.appendChild(descriptionPara);
+
+    // append the a new project to the projects container
+    projectsContainer.appendChild(sectionProject);
+  });
 })();
